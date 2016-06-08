a58697bd14144af8fa69fc8aba1d9b85b9059a17
Hector <rootdevelopper>
2016-06-07 23:08:22 -0500

    implemented basic doc and flesch score

--- MOOCTextEditor/src/document/Document.java
+++ MOOCTextEditor/src/document/Document.java
@@ -12,60 +12,104 @@ import java.util.regex.Pattern;
 public abstract class Document {
 
 	private String text;
-	
-	/** Create a new document from the given text.
-	 * Because this class is abstract, this is used only from subclasses.
-	 * @param text The text of the document.
+
+	/**
+	 * Create a new document from the given text. Because this class is
+	 * abstract, this is used only from subclasses.
+	 * 
+	 * @param text
+	 *            The text of the document.
 	 */
-	protected Document(String text)
-	{
+	protected Document(String text) {
 		this.text = text;
 	}
-	
-	/** Returns the tokens that match the regex pattern from the document 
-	 * text string.
-	 * @param pattern A regular expression string specifying the 
-	 *   token pattern desired
-	 * @return A List of tokens from the document text that match the regex 
-	 *   pattern
+
+	/**
+	 * Returns the tokens that match the regex pattern from the document text
+	 * string.
+	 * 
+	 * @param pattern
+	 *            A regular expression string specifying the token pattern
+	 *            desired
+	 * @return A List of tokens from the document text that match the regex
+	 *         pattern
 	 */
-	protected List<String> getTokens(String pattern)
-	{
+	protected List<String> getTokens(String pattern) {
 		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
-		
+
 		while (m.find()) {
 			tokens.add(m.group());
 		}
-		
+
 		return tokens;
 	}
-	
+
 	// This is a helper function that returns the number of syllables
-	// in a word.  You should write this and use it in your 
+	// in a word. You should write this and use it in your
 	// BasicDocument class.
 	// You will probably NOT need to add a countWords or a countSentences method
-	// here.  The reason we put countSyllables here because we'll use it again
+	// here. The reason we put countSyllables here because we'll use it again
 	// next week when we implement the EfficientDocument class.
-	protected int countSyllables(String word)
-	{
-		// TODO: Implement this method so that you can call it from the 
-	    // getNumSyllables method in BasicDocument (module 1) and 
-	    // EfficientDocument (module 2).
-	    return 0;
+	protected int countSyllables(String word) {
+		{
+
+			// TODO: Implement this method so that you can call it from the
+
+			// getNumSyllables method in BasicDocument (module 1) and
+
+			// EfficientDocument (module 2).
+
+			int count = 0;
+
+			String pattern = "[aeiouyAEIOUY]+";
+
+			Pattern tokSplitter = Pattern.compile(pattern);
+
+			Matcher m = tokSplitter.matcher(word);
+
+			String lastToken = "";
+
+			while (m.find()) {
+
+				count++;
+
+				lastToken = m.group();
+				System.out.println(lastToken);
+
+			}
+
+			if (count > 1 && word.charAt(word.length() - 1) == 'e' && lastToken.equals("e")) { // compare
+																								// to
+																								// "e"
+																								// not
+																								// to
+																								// 'e'
+
+				count--;
+
+			}
+
+			return count;
+
+		}
 	}
-	
-	/** A method for testing
+
+	/**
+	 * A method for testing
 	 * 
-	 * @param doc The Document object to test
-	 * @param syllables The expected number of syllables
-	 * @param words The expected number of words
-	 * @param sentences The expected number of sentences
-	 * @return true if the test case passed.  False otherwise.
+	 * @param doc
+	 *            The Document object to test
+	 * @param syllables
+	 *            The expected number of syllables
+	 * @param words
+	 *            The expected number of words
+	 * @param sentences
+	 *            The expected number of sentences
+	 * @return true if the test case passed. False otherwise.
 	 */
-	public static boolean testCase(Document doc, int syllables, int words, int sentences)
-	{
+	public static boolean testCase(Document doc, int syllables, int words, int sentences) {
 		System.out.println("Testing text: ");
 		System.out.print(doc.getText() + "\n....");
 		boolean passed = true;
@@ -73,53 +117,57 @@ public abstract class Document {
 		int wordsFound = doc.getNumWords();
 		int sentFound = doc.getNumSentences();
 		if (syllFound != syllables) {
-			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
-					+ ", expected " + syllables);
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound + ", expected " + syllables);
 			passed = false;
 		}
 		if (wordsFound != words) {
-			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
-					+ ", expected " + words);
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound + ", expected " + words);
 			passed = false;
 		}
 		if (sentFound != sentences) {
-			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
-					+ ", expected " + sentences);
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound + ", expected " + sentences);
 			passed = false;
 		}
-		
+
 		if (passed) {
 			System.out.println("passed.\n");
-		}
-		else {
+		} else {
 			System.out.println("FAILED.\n");
 		}
 		return passed;
 	}
-	
-	
+
 	/** Return the number of words in this document */
 	public abstract int getNumWords();
-	
+
 	/** Return the number of sentences in this document */
 	public abstract int getNumSentences();
-	
+
 	/** Return the number of syllables in this document */
 	public abstract int getNumSyllables();
-	
+
 	/** Return the entire text of this document */
-	public String getText()
-	{
+	public String getText() {
 		return this.text;
 	}
-	
+
 	/** return the Flesch readability score of this document */
-	public double getFleschScore()
-	{
-	    // TODO: Implement this method
-	    return 0.0;
+	public double getFleschScore() {
+		// TODO: Implement this method
+
+		// according to lecture Flesch score = 206.835 -1.015(words/sentences)-
+		// 84.6(syllabus/words)
+
+		float constant1 = 206.835f;
+		float constant2 = -1.015f;
+		float constant3 = -84.6f;
+
+		float score_1 = getNumWords() / getNumSentences();
+		float score_2 = getNumSyllables() / getNumWords();
+
+		float score = constant1 - constant2 * (score_1) - constant3 * (score_2);
+
+		return score;
 	}
-	
-	
-	
+
 }
